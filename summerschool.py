7d666a37007809bf21fefaaa14d2df96b7c492d0
dtward <dtward@cis.jhu.edu>
2018-05-09 14:22:40 -0400

    some formatting

--- summerschool.py
+++ summerschool.py
@@ -35,12 +35,12 @@ def load_surface(fname):
                 vertices[:,i-2] = [float(n) for n in line.split()]
             else:
                 faces[i-(nv+2),:] = [np.abs(int(n))-1 for n in line.split()]
-    return vertices,faces
+    return vertices, faces
 
 def axis_equal(ax=None):
     '''Set x,y,z axes to constant aspect ratio
     ax is a matplotlib 3d axex object'''
-    if ax is None: ax=plt.gca()
+    if ax is None: ax = plt.gca()
     xlim = ax.get_xlim()
     ylim = ax.get_ylim()
     zlim = ax.get_zlim()
@@ -54,3 +54,27 @@ def axis_equal(ax=None):
     ax.set_xlim(cx + np.array([-1,1])*0.5*r)
     ax.set_ylim(cy + np.array([-1,1])*0.5*r)
     ax.set_zlim(cz + np.array([-1,1])*0.5*r)
+    
+def plot_grid(X0, X1, rstride=1, cstride=1, ax=None, **kwargs):
+    ''' 
+    plot a grid defined by X0 and X1, 
+    which are 2d arrays that should be the same size
+    for example as output from meshgrid
+    
+    rstride defines how many rows to skip
+    cstride defines how many columns to skip
+    '''
+    if ax is None: ax = plt.gca()
+    # some defaults
+    defaultargs = {
+        'color':'k'
+    }
+    kwargs.update(defaultargs)
+    # plot rows
+    for i in range(0, X0.shape[0], rstride):
+        ax.plot(X0[i,:], X1[i,:], **kwargs)
+    if i < X0.shape[0]-1: ax.plot(X0[-1,:], X1[-1,:], **kwargs)
+    # plot columns
+    for j in range(0,X0.shape[1],cstride):
+        ax.plot(X0[:,j], X1[:,j], **kwargs)
+    if i < X0.shape[1]-1: ax.plot(X0[-1,:], X1[-1,:], **kwargs)
